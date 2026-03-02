d06000352e7731b012d91028a9be7f2d9fd8415a
arikanbono <arikan@bono.eco>
2026-03-02 22:07:45 +0300

    Initial Commit

---
+++ assets/website/app.js
@@ -0,0 +1,18 @@
+/*
+ * RSTheme Industrie – asset entry point
+ * CSS files are bundled through Webpack Encore.
+ * Vendor/plugin JS files are loaded via <script> tags (legacy global scope).
+ */
+
+// --- CSS imports ---
+import './css/vendor/bootstrap.min.css';
+import './css/vendor/fontawesome-pro.css';
+import './css/vendor/remixicon.css';
+import './css/vendor/animate.min.css';
+import './css/vendor/magnific-popup.css';
+import './css/vendor/odometer.min.css';
+import './css/vendor/spacing.css';
+import './css/plugins/swiper.min.css';
+import './css/plugins/nice-select.css';
+import './css/plugins/nouislider.min.css';
+import './scss/main.scss';
